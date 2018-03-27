766cf894a56301264b2a8502148dc5745e7b13cd
Enrique Rojas Reyes <erojasr@unicartagena.edu.co>
2018-03-27 02:27:59 -0500

    maqueta lista, falta probar todos lso numeros y agregar l apublicidad

---
+++ ruletav1/ruletav1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ruletav1
+{
+    
+	public partial class MainPage : ContentPage
+	{
+        
+        public MainPage()
+		{
+			InitializeComponent();
+            btncalcular.Clicked += Btncalcular_Clicked;   
+		}
+
+        private void Btncalcular_Clicked(object sender, EventArgs e)
+        {
+            var text = enumber.Text;
+            if (enumber.Text =="1")
+            {
+                progressBar1.Progress += 0.01;
+            }
+            if (enumber.Text == "2")
+            {
+                progressBar2.Progress += 0.01;
+            }
+            if (enumber.Text == "3")
+            {
+                progressBar3.Progress += 0.01;
+            }
+
+        }
+    }
+}
